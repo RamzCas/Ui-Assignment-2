dd994fdcad86016afbdb6d3906f705d51e7c02d5
RamzCas <2749414@students.wits.ac.za>
2024-05-07 11:27:22 +0200

    changed UI
    changed uI layout
    added board for back pack
    
      -need to add instancite tokens

--- Assets/Scripts/Items.cs
+++ Assets/Scripts/Items.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class Items : MonoBehaviour
 {
-    public GameObject Sword;
-    public GameObject Shield;
-    public GameObject Position;
-    public GameObject Armor;
+    //public GameObject Sword;
+    //public GameObject Shield;
+    //public GameObject Position;
+    //public GameObject Armor;
     void Start()
     {
-        Sword.SetActive(false);
-        Shield.SetActive(false);    
-        Position.SetActive(false);
-        Armor.SetActive(false);
+        //Sword.SetActive(false);
+        //Shield.SetActive(false);    
+        //Position.SetActive(false);
+        //Armor.SetActive(false);
     }
 
    
