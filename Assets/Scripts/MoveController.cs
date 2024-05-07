0f9ba7625b6ee4199b3336f451347b994959a8d4
RamzCas <2749414@students.wits.ac.za>
2024-05-07 20:39:19 +0200

    did shit aparently

--- Assets/Scripts/MoveController.cs
+++ Assets/Scripts/MoveController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class MoveController : MonoBehaviour
+public class MoveController : MonoBehaviour /*IBeginDragHandler, IDragHandler, IEndDragHandler*/
 {
     Vector3 offset;
     Collider2D collider2D;
@@ -16,34 +16,48 @@ public class MoveController : MonoBehaviour
 
     private void OnMouseDrag()
     {
-        transform.position = MousWorldPos() + offset;   
+        transform.position = MousWorldPos() + offset;
     }
 
-    private void OnMouseUp()
+    //private void OnMouseUp()
+    //{
+    //    collider2D.enabled = false;
+    //    var rayOrigin = Camera.main.transform.position;
+    //    var rayDirection = MousWorldPos() - Camera.main.transform.position;
+    //    RaycastHit2D hitInfo;
+
+    //    if (hitInfo = Physics2D.Raycast(rayOrigin, rayDirection))
+    //    {
+    //        if (hitInfo.transform.tag == TagPoint)
+    //        {
+    //            transform.position = hitInfo.transform.position + new Vector3(0, 0, -0.01f);
+    //        }
+    //    }
+
+    //    collider2D.enabled = true;
+    //}
+
+
+    Vector3 MousWorldPos()
     {
-        //collider2D.enabled = false;
-        //var rayOrigin = Camera.main.transform.position;
-        //var rayDirection = MousWorldPos() - Camera.main.transform.position;
-        //RaycastHit2D hitInfo;
-
-        //if (hitInfo = Physics2D.Raycast(rayOrigin, rayDirection)) 
-        //{
-        //    if (hitInfo.transform.tag == TagPoint)
-        //    {
-        //        transform.position = hitInfo.transform.position + new Vector3(0, 0, -0.01f);
-        //    }
-        //}
-            
-        //collider2D.enabled = true;
-    }
-
-
-    Vector3 MousWorldPos() 
-    {
-     var mouseScreenPos = Input.mousePosition;
+        var mouseScreenPos = Input.mousePosition;
         mouseScreenPos.z = Camera.main.WorldToScreenPoint(transform.position).z;
         return Camera.main.ScreenToWorldPoint(mouseScreenPos);
 
-    }
+     }
+        //public void OnBeginDrag(PointerEventData eventData)
+        //{
+        //    Debug.Log("DragBegin");
+        //}
 
-}
+        //public void OnDrag(PointerEventData eventData)
+        //{
+        //    Debug.Log("Dragging");
+        //    transform.position = Input.mousePosition;
+        //}
+
+        //public void OnEndDrag(PointerEventData eventData)
+        //{
+        //    Debug.Log("DragStop");
+        //}
+ }
